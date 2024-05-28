f8f2645da12bc41eb0f5c41244a88be3982fbdd8
OscarMendozaPablo <167824827+OscarMendozaPablo@users.noreply.github.com>
2024-05-28 16:13:04 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[]args){
+        interface CursoDAO {
+        }
+
+        interface CatedraticoDAO {
+        }
+
+        interface InscripcionDAO {
+        }
+
+        class CursoDAOImpl implements CursoDAO {
+        }
+
+        class CatedraticoDAOImpl implements CatedraticoDAO {
+        }
+
+        class InscripcionDAOImpl implements InscripcionDAO {
+        }
+
+        AlumnoDAO alumnoDAO = new AlumnoDAO();
+        CursoDAO cursoDAO = new CursoDAOImpl();
+        CatedraticoDAO catedraticoDAO = new CatedraticoDAOImpl();
+        InscripcionDAO inscripcionDAO = new InscripcionDAOImpl();
+
+        Alumno alumno = new Alumno("Juan", "Perez", "juan.perez@gamil.com");
+        alumnoDAO.registrarAlumno(alumno);
+
+    }
+}
 
     }
 }
