24d1a30b0ba691c7d68e2748b32854f2e9bcaa57
lesterGomez <160526532+lester-Gomez@users.noreply.github.com>
2024-05-29 21:51:44 -0600

    Merge branch 'main' of https://github.com/lester-Gomez/Proiyecto

--- Coneccion.java
+++ Coneccion.java
@@ -7,7 +7,7 @@ public class Coneccion {
 
         private static final String URL = "jdbc:mysql://localhost:3306/Universidad";
         private static final String USER = "root";
-        private static final String PASSWORD = "CarlosYovani32505896";
+        private static final String PASSWORD = "CarlosYovani3";
 
         static {
             try {
